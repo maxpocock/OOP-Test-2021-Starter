6d436c18c22c159f722cb2786198e6f0c7b7638c
maxpocock <c19323561@mytudublin.ie>
2021-04-21 20:57:50 +0100

    finalCommit

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -40,19 +40,21 @@ public class ScoreDisplay extends PApplet
 
 	public void printScores()
 	{
+		String noteType = notes.toString();
+		char c;
 		int length = notes.size();
-		String Type = "Quaver";
-		for (i = 0; i < length; i++)
+		String Type;
+		for (i = 1; i < length+1; i+=2)
 		{
-			
-			/*if (i.Note.duration == 1)
+			c = noteType.charAt(i);
+			if(c == 1)
 			{
 				Type = "Quaver";
 			}
 			else
 			{
 				Type = "Crotchet";
-			}*/
+			}
 			println(notes.get(i) + " " + Type);
 		}
 	}
@@ -66,6 +68,8 @@ public class ScoreDisplay extends PApplet
 		line(100, 275, 900, 275);
 		line(100, 300, 900, 300);
 	}
+
+	
 
 	public ArrayList<Note> getNotes() {
 		return notes;
@@ -95,12 +99,71 @@ public class ScoreDisplay extends PApplet
 
 	public void draw()
 	{
+		String noteType = notes.toString();
+		int length = notes.size();
+		int y = 325;
+		char c;
+
 		background(255);
 		staves();
+		drawNotes();
+
+		
 	}
 
-	void drawNotes()
+	public void drawNotes()
 	{
+		String noteType = notes.toString();
+		int length = notes.size();
+		int y = 325;
+		int x;
+		char c;
+
+		for (i = 0; i < length+1; i+=2)
+		{
+			c = noteType.charAt(i);
+
+			switch (c) {
+				case 'D':
+					x = 0;
+					break;
+				case 'E':
+					x = 1;
+					break;
+				case 'F':
+					x = 3;
+					break;
+				case 'G':
+					x = 4;
+					break;
+				case 'A':
+					x = 5;
+					break;
+				case 'B':
+					x = 6;
+					break;
+				case 'c':
+					x = 7;
+					break;
+				case 'd':
+					x = 8;
+					break;
+
+			if (x!=0)
+			{
+				x = x *12;
+				x = x + 100;
+			}
+			else
+			{
+				x = 112;
+			}
+					
+			circle(x, y, 25);
+			fill(0,0,0);
+			}
+			y = y - 25;
+		}
 	}
 
 	

5843496eef03f9b79d51ca6ff49ecf612587a0cf
maxpocock <c19323561@mytudublin.ie>
2021-04-21 17:47:43 +0100

    noteUpdates

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -10,7 +10,16 @@ public class ScoreDisplay extends PApplet
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
-	ArrayList notes;
+	ArrayList<String> notes;
+	public ArrayList<String> getNotes() {
+		return notes;
+	}
+
+	public void setNotes(ArrayList<String> notes) {
+		this.notes = notes;
+	}
+
+	int i;
 	
 	public void settings()
 	{
@@ -24,7 +33,7 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		loadScore();
 	}
 
 	public void draw()
@@ -39,11 +48,23 @@ public class ScoreDisplay extends PApplet
 
 	public void loadScore()
 	{
-		int i;
+		String x;
+		char y;
 
 		for (i = 0; i < score.length(); i++)
 		{
-			notes.add(score.charAt(i));
+			y = score.charAt(i);
+			x = String.valueOf(y);
+			notes.add(x);
+			//if (score.isDigit())
+		}
+	}
+
+	public void printScores()
+	{
+		for (i = 0; i < score.length(); i++)
+		{
+			println(notes.get(i));
 		}
 	}
 }
